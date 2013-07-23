84e9b4481256e412799b2e735735602e3e248aab
Pete <peterringelmann@gmail.com>
2013-07-23 14:19:48 +0000

    web.js update

--- web.js
+++ web.js
@@ -9,8 +9,8 @@ app.get('/', function(request, response) {
   //response.send('Hello World4!');
   
   var buffer = new Buffer(fs.readFileSync("index.html"));
-  buffer.toString();
-  response.send(buffer);
+  
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
